96faf85399bfe8bb16d2e3963703ac40d552547b
ifpr <isabelestachuk@gmail.com>
2023-03-09 12:26:05 -0300

    terminei os botoes

--- app/src/main/java/com/example/info3_23_02_23/MainActivity.java
+++ app/src/main/java/com/example/info3_23_02_23/MainActivity.java
@@ -9,6 +9,12 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button buttonCadastroCli;
+    Button buttonConsultar;
+    Button buttonEmprestar;
+    Button buttonComprar;
+    Button buttonReservar;
+    Button buttonAjuda;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +26,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick (View view) {
                 Intent in = new Intent(MainActivity.this, TelaActivity.class);
+                startActivity(in);
+
+            }
+        });
+
+        buttonConsultar=findViewById(R.id.buttonConsultar);
+        buttonConsultar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Intent in = new Intent(MainActivity.this, ConsultarActivity.class);
+                startActivity(in);
+
+            }
+        });
+
+        buttonEmprestar=findViewById(R.id.buttonEmprestar);
+        buttonEmprestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Intent in = new Intent(MainActivity.this, EmprestarActivity.class);
+                startActivity(in);
+
+            }
+        });
+
+        buttonComprar=findViewById(R.id.buttonComprar);
+        buttonComprar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Intent in = new Intent(MainActivity.this, ComprarActivity.class);
+                startActivity(in);
+
+            }
+        });
+
+        buttonReservar=findViewById(R.id.buttonReservar);
+        buttonReservar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Intent in = new Intent(MainActivity.this, ReservarActivity.class);
+                startActivity(in);
+
+            }
+        });
+
+        buttonAjuda=findViewById(R.id.buttonAjuda);
+        buttonAjuda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick (View view) {
+                Intent in = new Intent(MainActivity.this, AjudaActivity.class);
                 startActivity(in);
 
             }
