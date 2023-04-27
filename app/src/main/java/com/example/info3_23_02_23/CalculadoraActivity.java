1c2344df28a9d0d17be2eb7047ac9896aed426a6
ifpr <isabelestachuk@gmail.com>
2023-04-27 12:43:34 -0300

    Terminei a CalculadoraActivity.

--- app/src/main/java/com/example/info3_23_02_23/CalculadoraActivity.java
+++ app/src/main/java/com/example/info3_23_02_23/CalculadoraActivity.java
@@ -1,4 +1,4 @@
-package com.example.info3_23_02_23;
+﻿package com.example.info3_23_02_23;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,27 +12,36 @@ public class CalculadoraActivity extends AppCompatActivity {
     EditText txtNumero;
     EditText txtNumero2;
     TextView tvResultado;
-
-    Button bEnviar;
+    Button buttonSoma;
+    private double result;
+    double num1;
+    double num2;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculadora);
-        txtNumero =  findViewById(R.id.txtNumero);
+
+        txtNumero = findViewById(R.id.txtNumero);
         txtNumero2 = findViewById(R.id.txtNumero2);
         tvResultado = findViewById(R.id.tvResultado);
-        bEnviar = findViewById(R.id.bEnviar);
-        bEnviar.setOnClickListener(new View.OnClickListener(){
+        buttonSoma = findViewById(R.id.buttonSoma);
 
+        buttonSoma.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View view) {
+                num1 = Double.parseDouble(txtNumero.getText().toString());
+                num2 = Double.parseDouble(txtNumero2.getText().toString());
+                tvResultado = findViewById(R.id.tvResultado);
+                result = num1*num2;
+                tvResultado.setText(String.valueOf(result));
 
             }
         });
 
-        }
-
+    }
 }
+
+
 
